d716d02d9fcada92348e93bb9aa2ecd362e1e0ea
Tab <anhquang976@gmail.com>
2021-04-15 23:08:09 +0700

    Completed day 16

--- main.py
+++ main.py
@@ -1,75 +1,25 @@
-from datas import MENU, resources
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-# Coins
-penny = 0.01
-dime = 0.1
-nickel = 0.05
-quarter = 0.25
-
-payment_request = "You need to pay before receiving it"
-not_enough_ingredients = "Not enough ingredients"
-price = 0
-total = 0
-
-
-def resource():
-    for i in resources:
-        print(resources[i])
-
-
-def cost(_type):
-    global price
-    price = MENU[_type]['cost']
-    print(f"The price of this is: {MENU[_type]['cost']}")
-    return price
-
-
-def payment():
-    global total
-    _penny = int(input("Penny: "))
-    _dime = int(input("Dime: "))
-    _nickel = int(input("Nickel: "))
-    _quarter = int(input("Quarter: "))
-    total = (_penny * penny) + (_dime * dime) + (_nickel * nickel) + (_quarter * quarter)
-    return total
-
-
-def statistics(_type):
-    a = 0
-    ingredient = MENU[_type]["ingredients"]
-    for i in ingredient:
-        if resources[i] < ingredient[i]:
-            a += 1
-    if a > 0:
-        return not_enough_ingredients
-    else:
-        for i in ingredient:
-            resources[i] = resources[i] - ingredient[i]
-
-
-def deal(_type):
-    cost(_type)
-    print(payment_request)
-    payment()
-    if total == price:
-        print("Thank you. Bon appetite!")
-    elif total < price:
-        print("You don't have enough money")
-    else:
-        print(f"Here's your change: {total - price}")
-
+money = MoneyMachine()
 
 _continue = True
 while _continue:
-    type_coffee = input("What would you like? ").lower()
+    choice = menu.get_items()
+    type_coffee = input(f"What would you like? {choice}: ").lower()
     if type_coffee == "report":
-        resource()
+        coffee_maker.report()
     else:
-        if statistics(type_coffee) == not_enough_ingredients:
-            print(not_enough_ingredients)
-        else:
-            deal(type_coffee)
+        drink = menu.find_drink(type_coffee)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
     _continue = True
+
+
 
