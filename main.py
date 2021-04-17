c4fc655f919be3aa5e17c75c685cb924e00b274b
Tab <anhquang976@gmail.com>
2021-04-17 22:49:29 +0700

    Completed day 17

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 
-
 for i in question_data:
-    question = Question(i["text"], i["answer"])
+    question = Question(i["question"], i["correct_answer"])
     question_bank.append(question)
 
-quiz = QuizBrain(question_bank[0].text)
-quiz.question_number += 1
-print(f"Q.{quiz.question_number}: {quiz.question_list} (True/False)?: ")
+quiz = QuizBrain(question_bank)
 
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
