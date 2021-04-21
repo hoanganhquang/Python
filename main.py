d7de8861daa32e1f7b32eb9e66a8c3b8681a09d5
Tab <anhquang976@gmail.com>
2021-04-21 13:22:16 +0700

    Completed day 19

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import turtle
-from turtle import Turtle, Screen
+from turtle import  Turtle, Screen
 import random
 
-color_list = [(229, 228, 226), (225, 223, 224), (199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 224, 227), (186, 158, 53), (6, 57, 83), (109, 67, 85), (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47), (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186), (172, 153, 159), (212, 183, 177), (176, 198, 203)]
-turtle.colormode(255)
-
-tur = Turtle()
-tur.shape("circle")
-tur.setheading(225)
-tur.forward(300)
-tur.setheading(0)
-num = 100
-
-
-for i in range(1, num):
-    tur.dot(20, random.choice(color_list))
-    tur.penup()
-    tur.forward(50)
-
-    if i % 10 == 0:
-        tur.setheading(98)
-        tur.forward(50)
-        tur.setheading(180)
-        tur.forward(500)
-        tur.setheading(0)
-
-
-
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make you bet", prompt="Choose color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtle = []
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtle.append(new_turtle)
+
+if user_bet:
+    race = True
+
+while race:
+    for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            race=False
+            win = turtle.pencolor()
+            if win == user_bet:
+                print("You win")
+            else:
+                print("you lost")
+        ran_distance = random.randint(0, 10)
+        turtle.forward(ran_distance)
+
+
+
 screen.exitonclick()
+
