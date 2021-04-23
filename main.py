e735e129128d122d5cc23308148538fa3a32f387
Tab <anhquang976@gmail.com>
2021-04-23 19:16:44 +0700

    completed Snake game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Score
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("My snake game")
 screen.tracer(0)
 
 tim = Snake()
+food = Food()
+score = Score()
 
 screen.listen()
 screen.onkey(tim.up, "Up")
@@ -21,8 +25,16 @@ while on_game:
     screen.update()
     time.sleep(0.1)
     tim.move()
-
-
-
+    if tim.head.distance(food) < 15:
+        food.refresh()
+        score.total_score()
+        tim.extend()
+    if tim.head.xcor() > 280 or tim.head.xcor() < -280 or tim.head.ycor() > 280 or tim.head.ycor() < -280:
+        score.game_over()
+        on_game = False
+    for s in tim.segments[1:]:
+        if tim.head.distance(s) < 10:
+            on_game = False
+            score.game_over()
 screen.exitonclick()
 
--- snake.py
+++ snake.py
@@ -4,6 +4,7 @@ UP = 90
 DOWN = 270
 RIGHT = 0
 LEFT = 180
+START_POSITIONS = [(0, 0), (0, -20), (0, -40)]
 
 class Snake:
     def __init__(self):
@@ -12,14 +13,18 @@ class Snake:
         self.head = self.segments[0]
 
     def new_tim(self):
-        x = 0
-        for i in range(0, 3):
-            new_turtle = Turtle(shape="square")
-            new_turtle.color("white")
-            new_turtle.penup()
-            new_turtle.goto(x, 0)
-            x -= 20
-            self.segments.append(new_turtle)
+        for i in START_POSITIONS:
+            self.add_segment(i)
+
+    def add_segment(self, positions):
+        new_turtle = Turtle(shape="square")
+        new_turtle.color("white")
+        new_turtle.penup()
+        new_turtle.goto(positions)
+        self.segments.append(new_turtle)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
