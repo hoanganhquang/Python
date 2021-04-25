138f12f93c3aa59a59b2ec9d5c1821df82abb54f
Tab <anhquang976@gmail.com>
2021-04-25 15:49:10 +0700

    Pong game: add ball

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,23 +13,35 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+score = Scoreboard()
 
 
 screen.listen()
 screen.onkey(key="Up", fun=r_paddle.up)
 screen.onkey(key="Down", fun=r_paddle.down)
-screen.onkey(key="W", fun=l_paddle.up)
-screen.onkey(key="S", fun=l_paddle.down)
+screen.onkey(key="w", fun=l_paddle.up)
+screen.onkey(key="s", fun=l_paddle.down)
+
 
 on_game = True
 while on_game:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
-    if ball.ycor() == 270:
-        ball.wall()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.b_paddle()
 
 
+    if ball.xcor() > 380:
+        ball.game_reset()
+        score.l_point()
+
+    if ball.xcor() < -380:
+        ball.game_reset()
+        score.r_point()
 
 
 
