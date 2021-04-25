32d7c5600e6c8cd0e2f3acaf182e5854ccca1a19
Tab <anhquang976@gmail.com>
2021-04-25 15:49:59 +0700

    completed day 22

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ while on_game:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.b_paddle()
 
-
     if ball.xcor() > 380:
         ball.game_reset()
         score.l_point()
