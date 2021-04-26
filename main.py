57be6d5be828733255ec51864f67fc613ab99acb
Tab <anhquang976@gmail.com>
2021-04-26 23:42:54 +0700

    add car

--- main.py
+++ main.py
@@ -1,52 +1,24 @@
-from turtle import Screen
-from paddle import Paddle
-from ball import Ball
 import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=800, height=600)
-screen.bgcolor("black")
-screen.title("Pong")
+screen.setup(width=600, height=600)
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-ball = Ball()
-score = Scoreboard()
-
+player = Player()
+car = CarManager()
 
 screen.listen()
-screen.onkey(key="Up", fun=r_paddle.up)
-screen.onkey(key="Down", fun=r_paddle.down)
-screen.onkey(key="w", fun=l_paddle.up)
-screen.onkey(key="s", fun=l_paddle.down)
+screen.onkey(fun=player.move, key="Up")
 
-
-on_game = True
-while on_game:
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
     screen.update()
-    time.sleep(ball.move_speed)
-    ball.move()
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
 
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        ball.b_paddle()
-
-    if ball.xcor() > 380:
-        ball.game_reset()
-        score.l_point()
-
-    if ball.xcor() < -380:
-        ball.game_reset()
-        score.r_point()
-
-
-
-
-
-
-
-screen.exitonclick()
+    car.create_car()
+    car.move()
 
--- scoreboard.py
+++ scoreboard.py
@@ -1,27 +1,5 @@
-from turtle import Turtle
+FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.color("white")
-        self.hideturtle()
-        self.penup()
-        self.l_score = 0
-        self.r_score = 0
-        self.update()
-
-    def update(self):
-        self.clear()
-        self.goto(-100, 200)
-        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
-        self.goto(100, 200)
-        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
-
-    def l_point(self):
-        self.l_score += 1
-        self.update()
-
-    def r_point(self):
-        self.r_score += 1
-        self.update()
+class Scoreboard:
+    pass
