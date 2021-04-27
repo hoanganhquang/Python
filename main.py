01bda7dbd60d182dc8bc7c36d20a8fa4fb565261
Tab <anhquang976@gmail.com>
2021-04-28 00:24:59 +0700

    completed day 24

--- main.py
+++ main.py
@@ -1,52 +1,10 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
-import time
+with open("Input/Names/invited_names.txt") as file1:
+    name = file1.readlines()
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
-
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
-
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
-
-    #Detect collision with food.
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    #Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.reset()
-        snake.reset()
-
-    #Detect collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
-            scoreboard.reset()
-            snake.reset()
-
-
-
-
-
-screen.exitonclick()
+with open("Input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+    for i in name:
+        directory = f"Output/ReadyToSend/letter_for_{i.strip()}.docx"
+        new_letter = letter.replace("[name]", i)
+        with open(directory, mode="w") as file2:
+            file2.write(new_letter)
