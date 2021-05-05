5f66c47baf021c0d6955c656d7d3642e5cac7e95
Tab <anhquang976@gmail.com>
2021-05-05 20:58:38 +0700

    completed day 26

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import turtle
 import pandas
-from obj import Obj
 
-screen = turtle.Screen()
-screen.title("U.S. States Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
+data = pandas.read_csv("nato.csv")
 
-file = pandas.read_csv("50_states.csv")
-data = file.state
-true_answer = 0
-exist_obj = []
-remain = []
+new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-on_game = True
-while on_game:
-    answer_state = screen.textinput(title=f"{true_answer}/50 States Correct", prompt="What's another state's name?")
-    if answer_state == "exit":
-        break
-    else:
-        for _state in data:
-            if answer_state == _state.lower() and answer_state not in exist_obj:
-                true_answer += 1
-                data1 = file[file.state == _state]
-                obj = Obj(data1.x, data1.y, _state)
-                exist_obj.append(_state)
-            if true_answer == 50:
-                break
+name_input = input("Enter a word: ").upper()
 
-
-for answer in data:
-    if answer not in exist_obj:
-        remain.append(answer)
-
-pandas.DataFrame(remain).to_csv("states_to_learn.csv")
-
-
+new_name = [new_dict[word] for word in name_input]
+print(new_name)
