b57a4caa7dc9c5903c3c69b70d2772ed4352a82a
Tab <anhquang976@gmail.com>
2021-05-07 20:19:31 +0700

    completed day 27

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import tkinter
 
+
+def convert():
+    value = input_.get()
+    km = 1.609343 * float(value)
+    value_label.config(text=str(km))
+
+
 window = tkinter.Tk()
 window.title("My first GUI program")
-window.minsize(width=400, height=400)
+window.config(padx=20, pady=20)
 
-my_label = tkinter.Label(text="New Text")
-my_label.pack()
+equal_label = tkinter.Label(text="is equal to")
+equal_label.grid(row=1, column=0)
 
-def change_text():
-    a = input_.get()
-    my_label.config(text=a)
+value_label = tkinter.Label(text="0")
+value_label.grid(row=1, column=1)
 
+miles_label = tkinter.Label(text="Miles")
+miles_label.grid(row=0, column=2)
 
-button = tkinter.Button(text="click", command=change_text)
-button.pack()
+km_label = tkinter.Label(text="Km")
+km_label.grid(row=1, column=2)
 
-input_ = tkinter.Entry()
-input_.pack()
+button = tkinter.Button(text="Calculate", command=convert)
+button.grid(row=2, column=1)
+
+input_ = tkinter.Entry(width=20)
+input_.grid(row=0, column=1)
 
 
 
