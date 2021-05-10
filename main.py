0ddf95fa23a06efd82811506a829379f77ef29b6
Tab <anhquang976@gmail.com>
2021-05-10 18:41:45 +0700

    completed day 28

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 PINK = "#e2979c"
 RED = "#e7385b"
@@ -6,33 +7,92 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+BREAK_MIN = 5
+reps = 0
+timer = None
+
+
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    timer_label.config(text="Timer", fg="white")
+    canvas.itemconfig(timer_text, text=f"00:00")
+    check_mark.config(text="")
+    reps = 0
+
+
+def start_timer():
+    global reps
+    reps += 1
+    work = WORK_MIN * 60
+    short_break = BREAK_MIN * 60
+
+    if reps % 2 == 0:
+        countdown(short_break)
+        timer_label.config(text="Break", fg="white")
+        window.deiconify()
+    else:
+        countdown(work)
+        timer_label.config(text="Work", fg="white")
+
+
+def countdown(count):
+
+    _min = math.floor(count / 60)
+    _sec = count % 60
+    if _min < 10:
+        _min = f"0{_min}"
+    if _sec < 10:
+        _sec = f"0{_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{_min}:{_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for i in range(work_sessions):
+            mark += "✔"
+        check_mark.config(text=mark)
+
+        if len(mark) == int(input_.get()):
+            window.after_cancel(timer)
+            reset_timer()
+            check_mark.config(text=mark)
+
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=40, pady=50, bg=GREEN)
+window.config(bg=GREEN, padx=30)
+window.maxsize(730, 500)
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"))
+timer_label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg="white", bg=GREEN)
 timer_label.grid(row=0, column=1)
 
 canvas = Canvas(width=662, height=377, bg=GREEN, highlightthickness=0)
 tomato_img = PhotoImage(file="png-clipart-computer-icons-scalable-graphics-tomato-emoji-food-tomato-removebg-preview.png")
 canvas.create_image(331, 188.5, image=tomato_img)
-canvas.create_text(331, 215, text="00:00", fill="white", font=(FONT_NAME, 40, "bold"))
+timer_text = canvas.create_text(331, 215, text="00:00", fill="white", font=(FONT_NAME, 40, "bold"))
 canvas.grid(row=1, column=1)
 
-tick = Canvas(width=50, height=50, highlightthickness=0, bg=GREEN)
-tick.create_text(25, 25, text="✔", fill="white", font=(FONT_NAME, 30, "bold"))
-tick.grid(row=3, column=1)
+type_label = Label(text="Enter quantity", font=(FONT_NAME, 11, "bold"), fg="white", bg=GREEN)
+type_label.place(x=5, y=5)
 
-start_btn = Button(text="Start")
-start_btn.config(padx=20)
-start_btn.grid(row=2, column=0)
+input_ = Entry(width=5)
+input_.place(x=137, y=6)
 
-reset_btn = Button(text="Reset")
-reset_btn.config(padx=20)
-reset_btn.grid(row=2, column=2)
+start_btn = Button(text="Start", highlightthickness=0, command=start_timer, font=(FONT_NAME, 12, "bold"))
+start_btn.config(padx=20, pady=10)
+start_btn.place(x=50, y=400)
+
+reset_btn = Button(text="Reset", highlightthickness=0, command=reset_timer, font=(FONT_NAME, 12, "bold"))
+reset_btn.config(padx=20, pady=10)
+reset_btn.place(x=510, y=400)
+
+check_mark = Label(font=(FONT_NAME, 30), fg="white", bg=GREEN)
+check_mark.grid(row=2, column=1)
 
 window.mainloop()
 
