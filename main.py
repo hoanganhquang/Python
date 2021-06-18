b311413da9ca60a7b339ad4015142cd412e9a079
Tab <anhquang976@gmail.com>
2021-06-18 17:55:42 +0700

    completed day 61

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-from flask import Flask, render_template, request
-import requests
-import smtplib
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+from flask_bootstrap import Bootstrap
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(message="You must be at least 4 characters long"),
+                                             Length(min=4)])
+    password = PasswordField('Password', validators=[DataRequired(message="You must be at least 8 characters long"), Length(min=3)])
+    submit = SubmitField(label="Submit")
+
 
 app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "123"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@gmail.com" and login_form.password.data == "123":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+
+    return render_template('login.html', form=login_form)
 
 
 
-@app.route('/')
-def home_page():
-    data_posts = requests.get(url="https://api.npoint.io/9d1c9613b0bbd767e135")
-    data = data_posts.json()
-    return render_template('index.html', response=data)
-
-
-@app.route('/about')
-def about_page():
-    return render_template('about.html')
-
-
-@app.route('/contact', methods=['POST', 'GET'])
-def contact_page():
-    message = False
-    if request.method == 'POST':
-        message = True
-
-    return render_template('contact.html', message=message)
-
-
-@app.route('/post<int:num>')
-def to_post(num):
-    data_posts = requests.get(url="https://api.npoint.io/9d1c9613b0bbd767e135")
-    data = data_posts.json()
-    return render_template('post.html', post=data, num=num)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug="on")
+
