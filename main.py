078abf4828208b1d300c86d0daf4b0d98f762db4
Tab <anhquang976@gmail.com>
2021-06-18 22:06:43 +0700

    updated day 62

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
-from flask_bootstrap import Bootstrap
+import pandas
+
+file = pandas.read_csv('cafe-data.csv')
+print(file.iloc[[0]])
 
 
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(message="You must be at least 4 characters long"),
-                                             Length(min=4)])
-    password = PasswordField('Password', validators=[DataRequired(message="You must be at least 8 characters long"), Length(min=3)])
-    submit = SubmitField(label="Submit")
-
-
-app = Flask(__name__)
-Bootstrap(app)
-app.secret_key = "123"
-
-
-@app.route("/")
-def home():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=["POST", "GET"])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        if login_form.email.data == "admin@gmail.com" and login_form.password.data == "123":
-            return render_template('success.html')
-        else:
-            return render_template('denied.html')
-
-    return render_template('login.html', form=login_form)
-
-
-
-if __name__ == '__main__':
-    app.run(debug="on")
-
+# app = Flask(__name__)
+# Bootstrap(app)
+#
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+#
+#
+# @app.route('/cafes')
+# def cafes_page():
+#     file = pandas.read_csv('cafe-data.csv', index_col=0)
+#     file_table = file.to_string()
+#     return render_template('cafes.html', datatable=file_table)
+#
+#
+# @app.route('/add')
+# def add_page():
+#     return render_template('add.html')
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug="on")
