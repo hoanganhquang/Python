73a33735b7ccd759387382eb5b9b61139e35b0f1
Tab <anhquang976@gmail.com>
2021-06-20 22:23:47 +0700

    updated day 63

--- main.py
+++ main.py
@@ -1,50 +1,30 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
-from wtforms.validators import DataRequired, input_required, Length
-import pandas
-
-
-class CafeForm(FlaskForm):
-    cafe_name = StringField("Cafe name", validators=[DataRequired()])
-    cafe_location = StringField("Cafe Location on Google Maps (URL)", validators=[DataRequired()])
-    open_time = StringField("Opening Time e.g.8AM", validators=[DataRequired()])
-    close_time = StringField("Closing Time e.g.5:30PM", validators=[DataRequired()])
-    coffee_rating = SelectField("Coffee Rating", choices=[("✘"), ("☕"), ("☕☕"), ("☕☕☕"), ("☕☕☕☕"), ("☕☕☕☕☕")])
-    wifi = SelectField("Wifi Strength Rating", choices=[("✘"),("💪"), ("💪💪"), ("💪💪💪"), ("💪💪💪💪"), ("💪💪💪💪💪")])
-    power_socket = SelectField("Power Socket Availability",  choices=[("✘"),("🔌"), ("🔌🔌"), ("🔌🔌🔌"), ("🔌🔌🔌🔌"), ("🔌🔌🔌🔌🔌")])
-
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-Bootstrap(app)
 
+all_books = []
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    len_ = len(all_books)
+    return render_template("index.html", databook=all_books, leng=len_)
 
 
-@app.route('/cafes')
-def cafes_page():
-    file = pandas.read_csv('cafe-data.csv')
-    file_dict = file.to_dict().items()
-    file1 = file.iterrows()
-    return render_template('cafes.html', data=file_dict, data1=file1)
+@app.route("/add", methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        dict_ = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(dict_)
+        return redirect(url_for("home"))
 
-
-@app.route('/add', methods=["POST", "GET"])
-def add_page():
-    cafe_form = CafeForm()
-    cafe_form.validate_on_submit()
-    if cafe_form.validate_on_submit():
-        with open("cafe-data.csv", mode="a", encoding="utf-8") as file:
-            file.write(f"\n{cafe_form.cafe_name.data},{cafe_form.cafe_location.data},{cafe_form.open_time.data},{cafe_form.close_time.data},{cafe_form.coffee_rating.data},{cafe_form.wifi.data},{cafe_form.power_socket.data}")
-        redirect(url_for("cafes_page"))
-    return render_template('add.html', form=cafe_form)
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
-    app.run(debug="on")
+    app.run(debug=True)
+
