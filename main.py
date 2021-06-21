281357b2f1e200523ab360ba281b6d9d258345d0
Tab <anhquang976@gmail.com>
2021-06-21 16:36:36 +0700

    updated day 63

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-all_books = []
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.VARCHAR(200), nullable=False, unique=True)
+    author = db.Column(db.VARCHAR(200), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+db.create_all()
 
 
 @app.route('/')
 def home():
-    len_ = len(all_books)
-    return render_template("index.html", databook=all_books, leng=len_)
+    book_data = db.session.query(Book).all()
+    length = len(book_data)
+    return render_template("index.html", databook=book_data, leng=length)
 
 
 @app.route("/add", methods=["POST", "GET"])
 def add():
     if request.method == "POST":
-        dict_ = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(dict_)
+        book = Book(title=request.form["title"],
+                    author=request.form["author"],
+                    rating=request.form["rating"])
+        db.session.add(book)
+        db.session.commit()
+
         return redirect(url_for("home"))
 
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["POST", "GET"])
+def edit():
+    id_book = request.args.get("id_book")
+    book = Book.query.get(id_book)
+
+    if request.method == "POST":
+        rating_after = request.form["rating_after"]
+        book.rating = float(rating_after)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("edit.html", thisbook=book)
 
 
 if __name__ == "__main__":
