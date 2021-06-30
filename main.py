608a53b881ece582f3f80e894eaa1ed084ddfce3
Tab <anhquang976@gmail.com>
2021-06-30 21:56:17 +0700

    updated day 66

--- main.py
+++ main.py
@@ -1,77 +1,76 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-import requests
+import random
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'HFHGFH'
-Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies-data.db"
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class EditForm(FlaskForm):
-    your_rating = StringField("Your Rating Out of 10 e.g 7.5", validators=[DataRequired()])
-    your_review = StringField("Your Review", validators=[DataRequired()])
-
-
-class AddForm(FlaskForm):
-    movie_title = StringField("Movie Title", validators=[DataRequired()])
-
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, nullable=False, primary_key=True)
-    title = db.Column(db.VARCHAR(100), nullable=False, unique=True)
-    year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.VARCHAR(1000), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.VARCHAR(500), nullable=False)
-    img_url = db.Column(db.VARCHAR(1000), nullable=False)
-
-
-db.create_all()
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
 
 
 @app.route("/")
 def home():
-    data_movie = db.session.query(Movie).all()
-    leng = len(data_movie)
-    return render_template("index.html", data=data_movie, length=leng)
+    return render_template("index.html")
 
 
-@app.route("/edit<int:id_movie>", methods=["POST", "GET"])
-def edit(id_movie):
-    form = EditForm()
-    if form.validate_on_submit():
-        get_movie = Movie.query.get(id_movie)
-        get_movie.rating = float(form.your_rating.data)
-        get_movie.review = form.your_review.data
-        db.session.commit()
-
-        return redirect(url_for("home"))
-
-    return render_template("edit.html", form=form, id_send=id_movie)
-
-
-@app.route('/delete')
-def delete():
-    id_movie = request.args.get("id_movie")
-    delete_movie = Movie.query.get(id_movie)
-    db.session.delete(delete_movie)
-    db.session.commit()
-    return redirect(url_for('home'))
+# HTTP GET - Read Record
+@app.route('/random')
+def random_page():
+    all_cafes = db.session.query(Cafe).all()
+    one_cafe = random.choice(all_cafes)
+    return jsonify(cafe={"can_take_calls": one_cafe.can_take_calls,
+                         "coffee_price": one_cafe.coffee_price,
+                         "has_sockets": one_cafe.has_sockets,
+                         "has_toilet": one_cafe.has_toilet,
+                         "has_wifi": one_cafe.has_wifi,
+                         "id": one_cafe.id,
+                         "img_url": one_cafe.img_url,
+                         "location": one_cafe.location,
+                         "map_url": one_cafe.map_url,
+                         "name": one_cafe.name,
+                         "seats": one_cafe.seats
+                         })
 
 
-@app.route('/add')
-def add():
-    add_form = AddForm()
-    return render_template('add.html', form=add_form)
+@app.route("/all")
+def all_cafe():
+    all_cafes = db.session.query(Cafe).all()
+    for cafe in all_cafes:
+        return jsonify(cafe={cafe.id: {"can_take_calls": cafe.can_take_calls,
+                         "coffee_price": cafe.coffee_price,
+                         "has_sockets": cafe.has_sockets,
+                         "has_toilet": cafe.has_toilet,
+                         "has_wifi": cafe.has_wifi,
+                         "id": cafe.id,
+                         "img_url": cafe.img_url,
+                         "location": cafe.location,
+                         "map_url": cafe.map_url,
+                         "name": cafe.name,
+                         "seats": cafe.seats
+                         }})
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
 
 
 if __name__ == '__main__':
