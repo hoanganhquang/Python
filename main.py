ab7860ce7d821631a796446077e7c5f439e347a9
Tab <cbcbvc>
2021-07-05 23:05:16 +0700

    Completed day 67

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ''
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
